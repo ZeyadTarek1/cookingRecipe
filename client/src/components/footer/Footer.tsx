cf7f51d0046ab31681a029857a580a39d5478873
Zeyad <zeyadtaziz@gmail.com>
2022-04-27 21:27:41 +0200

    footer fix

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,28 +1,29 @@
 import React from "react";
+import "./Footer.css";
+import { ReactComponent as FacebookImg } from "../../img/facebook.svg";
+import { ReactComponent as TwitterImg } from "../../img/twitter.svg";
+import { ReactComponent as GithubImg } from "../../img/github.svg";
 
 const Footer = () => (
-    <footer className="page-footer font-small blue pt-4">
-        <div className="container-fluid text-center text-md-left">
-            <div className="row">
-                <div className="col-md-4 mt-md-4 mt-4">
-                    <h5 className="text-uppercase">Home</h5>
-                </div>
-
-                <hr className="clearfix w-100 d-md-none pb-0" />
-
-                <div className="col-md-4 mb-md-0 mb-4">
-                    <h5 className="text-uppercase">About</h5>
-                </div>
-
-                <div className="col-md-4 mb-md-4 mb-4">
-                    <h5 className="text-uppercase">Contact</h5>
-                </div>
-            </div>
+    <footer className="footer">
+        <div className="socials">
+            <FacebookImg className="socialImg" />
+            <TwitterImg className="socialImg" />
+            <GithubImg className="socialImg" />
         </div>
 
-        <div className="footer-copyright text-center py-3">
-            © 2020 Copyright:
-            <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+        <div className="footerNavigation">
+            <p className="footerText">Home</p>
+            <p>·</p>
+            <p className="footerText">Create Recipe</p>
+            <p>·</p>
+            <p className="footerText">About</p>
+            <p>·</p>
+            <p className="footerText">Contact</p>
+        </div>
+
+        <div className="copywrite">
+            <p>© 2022 Zeyad Tarek</p>
         </div>
     </footer>
 );
