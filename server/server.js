5559f75200c5d05e784cbfc29a1edc2e2567c269
Zeyad <zeyadtaziz@gmail.com>
2022-05-01 00:14:40 +0200

    dirname fix

--- server/server.js
+++ server/server.js
@@ -135,10 +135,10 @@ app.patch("/recipes/:id", upload.array("image", "data"), async (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(dirname, "client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
     });
 }
 
