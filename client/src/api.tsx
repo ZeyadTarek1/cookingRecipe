5bf2f37838b5ef18721341f8a86e25e96d34efe0
Zeyad Tarek <zeyadtaziz@gmail.com>
2023-06-01 13:49:16 +0300

    update link

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -3,7 +3,7 @@ import { defaultRecipe, recipeModel } from "./models";
 const link =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5000"
-        : "https://cooking-recipe-axj6wjt93-zeyadtarek1.vercel.app";
+        : "https://cooking-recipe-zeyadtarek1.vercel.app";
 
 export const getRecipes = async () => {
     try {
